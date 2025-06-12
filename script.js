7712c52bbaab82e0500ac48490dabab102c237bd
Sherlockrio <vilmoca@gmail.com>
2025-06-12 19:45:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -226,7 +226,7 @@ function atualizarFooter(localSelecionado) {
 
       // Adiciona evento de clique para abrir mapa completo
       googleMaps.onclick = function () {
-      window.location.href = anuncio.fullMapUrl;
+      window.location.href = "https://www.google.com/maps/place/Rio+de+Janeiro";
 };
 
       return;
