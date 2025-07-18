1f344e69a696b32b5f0e605ca1af048565336d37
Sherlockrio <vilmoca@gmail.com>
2025-07-18 19:21:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const eventos = [
   {
     titulo: "Tour na Fábrica da Three Monkeys (Local: Zona Sul)",
     link: "https://www.sympla.com.br/evento/tour-na-fabrica-da-three-monkeys/2813867?referrer=www.google.com",
-    imagem: " https://images.squarespace-cdn.com/content/v1/51ef2bf2e4b02bcf8471333b/1548975653637-3OLIBQG1CMETLNT86O92/three-monkeys.jpg?format=750w",
+    imagem: "https://images.squarespace-cdn.com/content/v1/51ef2bf2e4b02bcf8471333b/1548975653637-3OLIBQG1CMETLNT86O92/three-monkeys.jpg?format=750w",
     dia: "Sábado",
     horario: "Tarde",
     local: "Zona Sul",
@@ -11,8 +11,8 @@ const eventos = [
   },
   {
     titulo: "Rolé dos Favellados: Morro da Providência (Local: Centro)",
-    link: " https://www.sympla.com.br/evento/morro-da-providencia-a-primeira-favela-role-dos-favellados-22-de-fevereiro/2828393?referrer=www.google.com",
-    imagem: " https://www.cieds.org.br/storage/resized/posts/October2023-1050x/224bfa7d5901cc685ccd8be78e4cad87.png ",
+    link: "https://www.sympla.com.br/evento/morro-da-providencia-a-primeira-favela-role-dos-favellados-22-de-fevereiro/2828393?referrer=www.google.com",
+    imagem: "https://www.cieds.org.br/storage/resized/posts/October2023-1050x/224bfa7d5901cc685ccd8be78e4cad87.png",
     dia: "Sábado",
     horario: "Tarde",
     local: "Centro",
@@ -20,8 +20,8 @@ const eventos = [
   },
   {
     titulo: "Forro de Pife DESCALÇO (Local: Zona Sul)",
-    link: "https://www.sympla.com.br/evento/forro-de-pife-descalco-a-moda-e-fazer-cedinho-e-descalco/2837092 ",
-    imagem: "https://bafafa.com.br/images/artigos/forro_de_pife_rampa_21032024_065219.jpg ",
+    link: "https://www.sympla.com.br/evento/forro-de-pife-descalco-a-moda-e-fazer-cedinho-e-descalco/2837092",
+    imagem: "https://bafafa.com.br/images/artigos/forro_de_pife_rampa_21032024_065219.jpg",
     dia: "Sábado",
     horario: "Tarde",
     local: "Zona Sul",
@@ -29,8 +29,8 @@ const eventos = [
   },
   {
     titulo: "BLACK BOM FANTASY (Local: Zona Norte)",
-    link: "https://www.sympla.com.br/evento/black-bom-fantasy-22-2-renascenca-clube/2810937 ",
-    imagem: "https://odia.ig.com.br/_midias/jpg/2024/11/22/1200x750/1_baile_black_bom_3c-34661080.jpg ",
+    link: "https://www.sympla.com.br/evento/black-bom-fantasy-22-2-renascenca-clube/2810937",
+    imagem: "https://odia.ig.com.br/_midias/jpg/2024/11/22/1200x750/1_baile_black_bom_3c-34661080.jpg",
     dia: "Sábado",
     horario: "Tarde",
     local: "Zona Norte",
@@ -38,12 +38,12 @@ const eventos = [
   },
 ];
 
-// Dados completos dos anúncios
+// Dados completos dos anúncios (ATUALIZADOS)
 const anuncios = [
   {
     local: "Zona Sul",
-    logo: "https://cafelamas.com.br/wp-content/uploads/2020/08/cropped-favicon-cafe-lamas-165x165.png ",
-    mapThumbnail: "https://i.ibb.co/4wr3RyJ2/zona-sul-2.png ",
+    logo: "https://cafelamas.com.br/wp-content/uploads/2020/08/cropped-favicon-cafe-lamas-165x165.png",
+    mapThumbnail: "https://i.ibb.co/4wr3RyJ2/zona-sul-2.png",
     info: [
       "CAFÉ LAMAS",
       "Restaurante tradicional",
@@ -51,12 +51,12 @@ const anuncios = [
       "Tel: (21) 2556-0799",
       "Diariamente, de 11h à 1h",
     ],
-    site: "https://cafelamas.com.br "
+    site: "https://cafelamas.com.br"
   },
   {
     local: "Zona Norte",
-    logo: "https://i.imgur.com/1Ax0HIk.jpeg ",
-    mapThumbnail: "https://i.ibb.co/bM9XVsQX/zona-norte.png ",
+    logo: "https://i.imgur.com/1Ax0HIk.jpeg",
+    mapThumbnail: "https://i.ibb.co/bM9XVsQX/zona-norte.png",
     info: [
       "PESCADOS NA BRASA",
       "Restaurante comida do norte / Carimbó",
@@ -65,12 +65,12 @@ const anuncios = [
       "3ª à sáb de 11h às 21h30",
       "Dom 11h às 18h",
     ],
-    site: "https://www.instagram.com/pescadosnabrasa "
+    site: "https://www.instagram.com/pescadosnabrasa"
   },
   {
     local: "Centro",
-    logo: "https://static.ifood-static.com.br/image/upload/t_thumbnail/logosgde/7602fa23-656e-4c11-9c93-7e6117a914c4/202308101032_Eyd3_i.jpg ",
-    mapThumbnail: "https://i.ibb.co/533gcXw/centro.png ",
+    logo: "https://static.ifood-static.com.br/image/upload/t_thumbnail/logosgde/7602fa23-656e-4c11-9c93-7e6117a914c4/202308101032_Eyd3_i.jpg",
+    mapThumbnail: "https://i.ibb.co/533gcXw/centro.png",
     info: [
       "AL FARABI",
       "Restaurante, botequim e eventos",
@@ -79,12 +79,12 @@ const anuncios = [
       "5a e 6a: 12h às 22h;",
       "Sáb e dom: 12h às 20h",
     ],
-    site: "https://www.instagram.com/alfa_bar_e_cultura "
+    site: "https://www.instagram.com/alfa_bar_e_cultura"
   },
   {
     local: "Zona Oeste",
-    logo: "https://espaconutriluz.com.br/wp-content/uploads/2022/04/logo-nutriluz.png ",
-    mapThumbnail: "https://i.ibb.co/20SFLhbj/zona-oeste.png ",
+    logo: "https://espaconutriluz.com.br/wp-content/uploads/2022/04/logo-nutriluz.png",
+    mapThumbnail: "https://i.ibb.co/20SFLhbj/zona-oeste.png",
     info: [
       "ESPAÇO NUTRI LUZ",
       "Espaço de bem estar",
@@ -92,7 +92,7 @@ const anuncios = [
       "Tel: (21) 96540-3854",
       "De 4ª à dom em diferentes horários",
     ],
-    site: "https://www.instagram.com/espaco_nutri_luz "
+    site: "https://www.instagram.com/espaco_nutri_luz"
   },
 ];
 
@@ -162,17 +162,19 @@ function filtrarEventos() {
   atualizarFooter(local);
 }
 
-// Função para atualizar o footer
+// Função para atualizar o footer - VERSÃO FINAL CORRIGIDA
 function atualizarFooter(localSelecionado) {
   const footerTopLeft = document.getElementById("footerTopLeft");
   const footerTopRight = document.getElementById("footerTopRight");
   const footerBottomLeft = document.getElementById("footerBottomLeft");
   const footerBottomRight = document.getElementById("footerBottomRight");
+  const locationIcon = document.getElementById("location-icon");
 
-  // Limpa apenas as seções necessárias
+  // Limpar completamente o conteúdo antes de atualizar
   footerTopLeft.innerHTML = '';
   footerTopRight.innerHTML = '';
   footerBottomLeft.innerHTML = '';
+  footerBottomRight.innerHTML = '';
 
   if (localSelecionado) {
     const anuncio = anuncios.find((a) => a.local === localSelecionado);
@@ -200,17 +202,25 @@ function atualizarFooter(localSelecionado) {
           <img src="${anuncio.mapThumbnail}" alt="Anunciantes ${anuncio.local}" class="map-thumbnail">
         </a>
       `;
+
+      return;
     }
-  } else {
-    // Restaura o conteúdo padrão do footer
-    footerTopLeft.innerHTML =
-      '<img src="https://i.imgur.com/SAidmdv.png " alt="Megafone" id="footerLogo">';
-    footerTopRight.innerHTML =
-      "Clique no filtro LOCAL para ver<br/>os principais anunciantes da região<br/>";
-    footerBottomLeft.innerHTML =
-      "Selecionando um LOCAL você encontra<br/>todos os anunciantes da região<br/>";
-    // O ícone de localização permanece no HTML, então não recriamos aqui
   }
+
+  // CORREÇÃO: Restaurar o footer estático COMPLETO com o ícone de localização
+  footerTopLeft.innerHTML =
+    '<img src="https://i.imgur.com/SAidmdv.png" alt="Megafone" id="footerLogo">';
+  footerTopRight.innerHTML =
+    "Clique no filtro LOCAL para ver<br/>os principais anunciantes da região<br/>";
+  footerBottomLeft.innerHTML =
+    "Selecionando um LOCAL você encontra<br/>todos os anunciantes da região<br/>";
+  
+  // Restaurar o ícone de localização
+  footerBottomRight.innerHTML = `
+    <div id="location-icon">
+      <img src="https://i.imgur.com/SAidmdv.png" alt="Ícone de Localização">
+    </div>
+  `;
 }
 
 // Event listeners
