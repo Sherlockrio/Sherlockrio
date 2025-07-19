7aab3e99e75617ec18cafa778e36b93dbcb74407
Sherlockrio <vilmoca@gmail.com>
2025-07-19 00:41:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -215,10 +215,7 @@ function atualizarFooter(localSelecionado) {
   footerBottomLeft.innerHTML =
     "Selecionando um LOCAL você encontra<br/>todos os anunciantes da região<br/>";
   footerBottomRight.innerHTML = 
-    `<div id="location-icon">
-      <img src="https://i.imgur.com/SAidmdv.png" alt="Ícone de Localização">
-    </div>
-  `;
+    `<img src="https://i.imgur.com/SAidmdv.png" alt="Ícone de Localização" id="location-icon">`;
 }
 
 // Event listeners
